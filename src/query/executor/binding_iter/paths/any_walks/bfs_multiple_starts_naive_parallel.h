3053026bfc2109542b90919a650bbe125655b967
Mateusz <matiutd888@gmail.com>
2025-02-22 19:44:54 +0100

    implement ms bfs parallel

--- src/query/executor/binding_iter/paths/any_walks/bfs_multiple_starts_naive_parallel.h
+++ src/query/executor/binding_iter/paths/any_walks/bfs_multiple_starts_naive_parallel.h
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include <memory>
@@ -11,91 +12,90 @@
 #include "query/executor/binding_iter/paths/any_walks/search_state.h"
 #include "query/executor/binding_iter/paths/index_provider/path_index.h"
 #include "query/parser/paths/automaton/rpq_automaton.h"
+#include "search_state_multiple_starts.h"
 
-#include "debug_mati.h"
+namespace Paths
+{
+    namespace Any
+    {
 
-namespace Paths { namespace Any {
+        /*
+        BFSMultipleStartsNaiveParallel returns a single path to all
+        reachable nodes from a starting node, using BFS.
+        */
+        template <bool MULTIPLE_FINAL>
+        class BFSMultipleStartsNaiveParallel : public BindingIter
+        {
+        private:
+            // Attributes determined in the constructor
+            VarId path_var;
+            std::vector<Id> start_nodes;
+            VarId end;
+            const RPQ_DFA automaton;
+            std::unique_ptr<IndexProvider> provider;
 
-/*
-BFSMultipleStartsNaive
-*/
-template <bool MULTIPLE_FINAL>
-class BFSMultipleStartsNaive : public BindingIter {
-private:
-    bool single_next();
-    void single_reset(); 
-    void set_new_start_node();
-    // Attributes determined in the constructor
-    VarId         path_var;
-    std::vector<Id> start_nodes;
-    VarId         end;
-    const RPQ_DFA automaton;
-    std::unique_ptr<IndexProvider> provider;
+            // where the results will be written, determined in begin()
+            Binding *parent_binding;
 
-    std::queue<ObjectId> start_nodes_q;
-    ObjectId current_start_node;
-    // where the results will be written, determined in begin()
-    Binding* parent_binding;
+            // Set of visited MultiSourceSearchStates
+            boost::unordered_node_set<MultiSourceMultiSourceSearchState, std::hash<MultiSourceMultiSourceSearchState>> visited;
 
-    // Set of visited SearchStates
-    boost::unordered_node_set<SearchState, std::hash<SearchState>> visited;
+            // Queue for BFS. Pointers point to the states in visited
+            std::queue<const MultiSourceMultiSourceSearchState *> open;
 
-    // Queue for BFS. Pointers point to the states in visited
-    std::queue<const SearchState*> open;
+            // Iterator for current node expansion
+            std::unique_ptr<EdgeIter> iter;
 
-    // Iterator for current node expansion
-    std::unique_ptr<EdgeIter> iter;
+            // The index of the transition being currently explored
+            uint_fast32_t current_transition;
 
-    // The index of the transition being currently explored
-    uint_fast32_t current_transition;
+            // true in the first call of next() and after a reset()
+            bool first_next = true;
 
-    // true in the first call of next() and after a reset()
-    bool first_next = true;
+            // Template type for storing nodes reached with a final state
+            typename std::conditional<MULTIPLE_FINAL,
+                                      boost::unordered_flat_set<uint64_t>,
+                                      DummySet>::type reached_final;
 
-    // Template type for storing nodes reached with a final state
-    typename std::conditional<MULTIPLE_FINAL,
-                              boost::unordered_flat_set<uint64_t>,
-                              DummySet>::type reached_final;
+        public:
+            // Statistics
+            uint_fast32_t idx_searches = 0;
 
-public:
-    // Statistics
-    uint_fast32_t idx_searches = 0;
+            BFSMultipleStartsNaiveParallel(
+                VarId path_var,
+                std::vector<Id> start_nodes,
+                VarId end,
+                RPQ_DFA automaton,
+                std::unique_ptr<IndexProvider> provider) : path_var(path_var),
+                                                           start(start),
+                                                           end(end),
+                                                           automaton(automaton),
+                                                           provider(std::move(provider)) {}
 
-    BFSMultipleStartsNaive(
-        VarId                          path_var,
-        std::vector<Id> start_nodes,
-        VarId                          end,
-        RPQ_DFA                        automaton,
-        std::unique_ptr<IndexProvider> provider
-    ) :
-        path_var      (path_var),
-        start_nodes         (start_nodes),
-        end           (end),
-        automaton     (automaton),
-        provider      (std::move(provider)) { }
+            void accept_visitor(BindingIterVisitor &visitor) override;
+            void _begin(Binding &parent_binding) override;
+            void _reset() override;
+            bool _next() override;
 
+            // Expand neighbors from current state
+            const MultiSourceSearchState *expand_neighbors(const MultiSourceSearchState &current_state);
 
-    void accept_visitor(BindingIterVisitor& visitor) override;
-    void _begin(Binding& parent_binding) override;
-    void _reset() override;
-    bool _next() override;
+            void assign_nulls() override
+            {
+                parent_binding->add(end, ObjectId::get_null());
+                parent_binding->add(path_var, ObjectId::get_null());
+            }
 
-    // Expand neighbors from current state
-    const SearchState* expand_neighbors(const SearchState& current_state);
+            // Set iterator for current node + transition
+            inline void set_iter(const MultiSourceSearchState &s)
+            {
+                // Get current transition object from automaton
+                auto &transition = automaton.from_to_connections[s.automaton_state][current_transition];
 
-    void assign_nulls() override {
-        parent_binding->add(end, ObjectId::get_null());
-        parent_binding->add(path_var, ObjectId::get_null());
+                // Get iterator from custom index
+                iter = provider->get_iter(transition.type_id.id, transition.inverse, s.node_id.id);
+                idx_searches++;
+            }
+        };
     }
-
-    // Set iterator for current node + transition
-    inline void set_iter(const SearchState& s) {
-        // Get current transition object from automaton
-        auto& transition = automaton.from_to_connections[s.automaton_state][current_transition];
-
-        // Get iterator from custom index
-        iter = provider->get_iter(transition.type_id.id, transition.inverse, s.node_id.id);
-        idx_searches++;
-    }
-};
-}} // namespace Paths::Any
+} // namespace Paths::Any
