51b6f24e41e897f53e5e6d10a7f78f6c8b0b032a
Mateusz <matiutd888@gmail.com>
2025-03-02 17:04:02 +0100

    improve run grid script

--- experiments/soc-pokec-relationships/run_grid_experiments.sh
+++ experiments/soc-pokec-relationships/run_grid_experiments.sh
@@ -33,12 +33,15 @@ fi
 ms_strategy=$1
 
 # Define parameters for both scripts
-server_params=(start0 start1)
-query_params=(1 2)
+server_params=(start0 start1 start2)
+declare -A query_params_dict
+query_params_dict["start0"]="1 2 3 4 5"
+query_params_dict["start1"]="1 2 3 4"
+query_params_dict["start2"]="1 2 3"
 
 # Create results directory with timestamp
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
-results_dir="results/experiment-$timestamp"
+results_dir="results/experiment-$ms_strategy-$timestamp"
 mkdir -p "$results_dir"
 
 echo "Saving to $results_dir"
@@ -46,7 +49,7 @@ echo "Saving to $results_dir"
 # Output files
 execution_durations_file="$results_dir/$ms_strategy-execution-durations.csv"
 visited_nodes_file="$results_dir/$ms_strategy-visited-nodes-counter.csv"
-query_times_file="$results_dir/$ms_strategy-query-times.csv"
+# query_times_file="$results_dir/$ms_strategy-query-times.csv"
 
 # Clear previous results
 echo "Server Param,Query Param,Execution Duration" > "$execution_durations_file"
@@ -63,15 +66,18 @@ for s in "${server_params[@]}"; do
     sleep 1
 
     # Iterate over query parameters
-    for q in "${query_params[@]}"; do
+    for q in ${query_params_dict[$s]}; do
         echo "Executing ./run_max_path_query.sh $q;"
-        { time ./run_max_path_query.sh "$q"; } >/dev/null 2> "$results_dir/time_output.txt"
+        { time ./run_max_path_query.sh "$q"; } >/dev/null 2>> "$results_dir/time_output.txt"
         query_time=$(cat "$results_dir/time_output.txt" | tr '\n' ' ')
         echo "Executed query with param $q while server param was $ms_strategy $s"
     done
     sleep 2
     stop_server_script
-    grep -oE 'Finished bfs with counter: Counter \[visited-nodes-counter\]: [0-9]+' "$log_file" | awk -v s="$s" -v q="$q" '{print s","q","$NF}' >> "$visited_nodes_file"
-    grep -oE 'Execution duration: [0-9]+' "$log_file" | awk -v s="$s" -v q="$q" '{print s","q","$NF}' >> "$execution_durations_file"
+    grep -oE 'Finished bfs with counter: Counter \[visited-nodes-counter\]: [0-9]+' "$log_file" | awk -v s="$s" -v q="$q" '{print s","NR","$NF}' >> "$visited_nodes_file"
+    grep -oE 'Execution duration: [0-9]+' "$log_file" | awk -v s="$s" -v q="$q" '{print s","NR","$NF}' >> "$execution_durations_file"
 
 done
+
+python transform_experiment_results.py $execution_durations_file
+python transform_experiment_results.py $visited_nodes_file
